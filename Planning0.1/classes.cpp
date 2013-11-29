883b0b94dafd3eacdc7a4e2d9c13ba71b3e6daee
elysiunk <christo.julien@gmail.com>
2013-11-29 21:51:39 +0100

    Methodes de classes.h implémenté dans le fichier classes.cpp

---
+++ Planning0.1/classes.cpp
@@ -0,0 +1,94 @@
+#include "classes.h"
+
+using namespace std;
+
+/************************/
+/*      Lecture         */
+/************************/
+int lecture::_id =0;
+
+lecture::lecture() : _id_course(-1),_id_prof(-1) {
+    _id++;
+}
+
+lecture::lecture(int id_course, int id_prof, int id_week):
+_id_course(id_course),_id_prof(id_prof), _id_week(id_week) {
+    _id++;
+}
+
+
+/*********************/
+/*      Week         */
+/*********************/
+week::week() : _id(0), _id_promo(0) {
+    _lectures.resize(22);
+}
+
+week::week(int id, int id_promo) : _id(id),_id_promo(id_promo){
+    _lectures.resize(22);
+}
+
+int week::get_id() {
+    return _id;
+}
+
+void week::add_lecture(int index, lecture l) {
+    this->_lectures.insert(_lectures.begin()+index,l);
+}
+
+
+
+/***********************/
+/*      Course         */
+/***********************/
+course::course() : _id(-1), _name("unknown"), _nb_lectures(-1){}
+
+course::course(int id, string name, int nb_lectures) 
+:_id(id), _name(name), _nb_lectures(nb_lectures){}
+
+int course::get_id() {
+    return _id;
+}
+
+string course::get_name() {
+    return _name;
+}
+
+
+/**********************/
+/*      Prof         */
+/*********************/
+prof::prof() :_id(-1), _name("unknown"){
+    assert(_availabiliy.size()==22);
+}
+
+prof::prof(int id, string name, vector<int> availability) :
+_id(id),_name(name), _availabiliy(availability){
+    assert(_availabiliy.size()==22);
+}
+
+void prof::grant_lecture(course c, week& w, int index) {
+    assert(_availabiliy.at(index));
+    this->_availabiliy.at(index)=0;
+
+    lecture l(c.get_id(), this->_id, w.get_id());
+    w.add_lecture(index,l);
+}
+
+
+
+/**********************/
+/*      Promo         */
+/**********************/
+promo::promo() :_id(-1), _name("unkonwn"), _nb_students(-1) {}
+
+promo::promo(int id, string name, int nb_students):
+_id(id), _name(name), _nb_students(nb_students){}
+
+void promo::add_course(course c) {
+    _courses[c.get_id()] = c;
+}
+
+course promo::get_course(int id) {
+    return _courses[id];
+}
--- Planning0.1/classes.h
+++ Planning0.1/classes.h
@@ -14,137 +14,84 @@
 #include <cassert>
 #include <map>
 
-using namespace std;
+
 
 class lecture {
     
     int _id_course;
     int _id_prof;
     int _id_week;
-    
 
 public:
         
     static int _id;
-    lecture():_id_course(-1),_id_prof(-1){
-        _id++;
-    }
+    lecture();
+    lecture(int id_course, int id_prof, int id_week);
     
-    lecture(int id_course, int id_prof, int id_week):_id_course(id_course),_id_prof(id_prof), _id_week(id_week){
-        _id++;
-    }  
-
-
 };
 
 class week {
+    
     //One week per promo and per week in the year
     int _id;
     int _id_promo;
-
-    vector<lecture> _lectures;
+    std::vector<lecture> _lectures;
 
 public:
 
-    week() : _id(0), _id_promo(0) {
-        _lectures.resize(22);
-
-    }
-
-    week(int id, int id_promo):_id(id),_id_promo(id_promo){
-
-        _lectures.resize(22);
-    }
+    week();
+    week(int id, int id_promo);
+    int get_id();
+    void add_lecture(int index, lecture l);
     
-    int get_id(){
-        return _id;
-    }
-    
-    void add_lecture(int index, lecture l){
-        this->_lectures.insert(_lectures.begin()+index,l);
-    }
-
-
 };
 
 class course {
+    
     int _id;
-    string _name;
+    std::string _name;
     int _nb_lectures;
     
 public:
     
-    course():_id(-1), _name("unknown"), _nb_lectures(-1){
-    }
-    
-    course(int id, string name, int nb_lectures):_id(id), _name(name), _nb_lectures(nb_lectures){
-    }
-    
-    int get_id(){
-        return _id;
-    }
-    
-    string get_name(){
-        return _name;
-    }
+    course();
+    course(int id, std::string name, int nb_lectures);
+    int get_id();
+    std::string get_name();
 
 };
 
 class prof{
+    
     int _id;
     std::string _name;
-    vector<int> _availabiliy;
-    map<int, course> _given_courses;
+    std::vector<int> _availabiliy;
+    std::map<int, course> _given_courses;
   
 public:
-    prof():_id(-1), _name("unknown"){
-        assert(_availabiliy.size()==22);
-    }
     
-    prof(int id, string name, vector<int> availability):
-    _id(id),_name(name), _availabiliy(availability){
-        assert(_availabiliy.size()==22);
-    }
-    
-    //index = cr�neaux
-    void grant_lecture(course c, week &w, int index){
-       
-        assert(_availabiliy.at(index));
-        this->_availabiliy.at(index)=0;
-        
-        lecture l(c.get_id(), this->_id, w.get_id());
-        w.add_lecture(index,l);
-    }
-            
+    prof();
+    prof(int id, std::string name, std::vector<int> availability);
+    //index = creneaux
+    void grant_lecture(course c, week &w, int index);
 
 };
 
 class promo{
+    
     int _id;
     int _nb_students;
-    string _name;
+    std::string _name;
     std::map<int, course> _courses;
     std::map<int, week> _weeks;
    
 public:
-    promo():_id(-1), _name("unkonwn"), _nb_students(-1) {
-    }
     
-    promo(int id, string name, int nb_students):
-    _id(id), _name(name), _nb_students(nb_students){
-    }
-    
-    void add_course(course c){
-        _courses[c.get_id()] = c;
-    }
-    
-    course get_course(int id){
-        return _courses[id];
-    }
-    
+    promo();
+    promo(int id, std::string name, int nb_students);
+    void add_course(course c);
+    course get_course(int id);
     
 };
 
-
 #endif	/* CLASSES_H */
-
