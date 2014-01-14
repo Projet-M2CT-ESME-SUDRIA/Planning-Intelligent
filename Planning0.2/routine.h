01126792d0ebce5053a46c18193c08ca4449273f
elysiunk <christo.julien@gmail.com>
2014-01-14 19:11:52 +0100

    Split de la routine pour placer les cours sur l'ensemble des semaines où il doit être

--- Planning0.2/routine.h
+++ Planning0.2/routine.h
@@ -17,6 +17,12 @@
 bool sort_by_availability(prof a, prof b);
 void rout1(std::vector<prof> &profs, std::vector<promo> &promos);
 void rout2(std::vector<prof> &profs, std::vector<promo> &promos);
+int cmpt_avail_prof(prof &p, int nb_dispo_prof, int week, int index);
+int cmpt_avail_promo (promo &p, int nb_dispo_promo, int week, int index);
+void grant_lectures(prof &p, promo &c);
+int get_match_prof_promo(prof &p, promo &c, int nb_hours);
+void display_weeks(promo p);
+
 
 #endif	/* ROUTINE_H */
 
