581f4898758c0415c879c0baba36b5eee679af92
elysiunk <christo.julien@gmail.com>
2014-01-14 14:28:38 +0100

    Modification des méthodes par rapport aux cours qui sont donnés qui ne sont plus une map mais un vecteur + ajout d'une méthode pour savoir si un prof est libre sur une semaine à un certain créneau

--- Planning0.2/prof.cpp
+++ Planning0.2/prof.cpp
@@ -10,7 +10,7 @@ prof::prof() :_name("unknown"){
     _id=_static_id++;
 }
 
-prof::prof(string name, map<int, vector<int> > availability, map<int, course> given_courses) : _name(name), _availability(availability), _given_courses(given_courses){
+prof::prof(string name, map<int, vector<int> > availability, vector<course> given_courses) : _name(name), _availability(availability), _given_courses(given_courses){
     _id=_static_id++;
 }
 
@@ -29,39 +29,46 @@ void prof::set_availability(int num_week, int index) {
 }
 
 void prof::add_given_course(course c){
-    _given_courses[c.get_id()] = c;
+    _given_courses.push_back(c);
 }
 
-void prof::grant_lecture(course c, week& w, int index) {
+int prof::grant_lecture(course c, week &w, int index) {
     vector<int> availability = this->get_availability(w.get_num_week());
     if (w.checkAvailability(index)) {
         
-        assert(availability.at(index));
+        //assert(availability.at(index));
         this->set_availability(w.get_num_week(), index);
 
         lecture l(c.get_id(), this->_id, w.get_id());
         w.add_lecture(index,l);
         cout << "Cours ajoute au prof et a la classe" << endl;
+        return 1;
     }
-    else
+    else {
         cout << "La classe a deja un cours" << endl;
+        return 0;
+    }
 }
 
 string prof::get_name() {
     return _name;
 }
 
-map<int, course> prof::get_given_courses() {
+vector<course> prof::get_given_courses() {
     return _given_courses;
 }
 
 void prof::list_profs() {
     int i;
-    map<int,course> courses = get_given_courses();
+    vector<course> courses = get_given_courses();
     cout << "Professeur numero " << _id << endl;
     cout << "\t Nom : " << _name << endl;
     cout << "\t Cours : " << endl;
     for(i = 0 ; i < courses.size() ; i++) { 
-        cout << "\t\t" << courses[i].get_name() << endl;
+        cout << "\t\t" << courses.at(i).get_name() << endl;
     }
+}
+
+int prof::is_available(int num_week, int index) {
+    return _availability[num_week].at(index);
 }
