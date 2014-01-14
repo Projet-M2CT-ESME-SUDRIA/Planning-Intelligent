5e4b5096c4734006e3f54ef4cadfa58177d992e0
elysiunk <christo.julien@gmail.com>
2014-01-14 14:26:57 +0100

    Changement des méthodes par rapport aux cours suivi qui n'est plus une map mais un vecteur + ajout de méthode pour récupérer une semaine

--- Planning0.2/promo.cpp
+++ Planning0.2/promo.cpp
@@ -10,13 +10,13 @@ promo::promo() :_name("unkonwn"), _nb_students(-1) {
     _id=_static_id++;
 }
 
-promo::promo(string name, int nb_students, map<int, course> courses):
+promo::promo(string name, int nb_students, vector<course> courses):
 _name(name), _nb_students(nb_students), _courses(courses){
     _id=_static_id++;
 }
 
 void promo::add_course(course c) {
-    _courses[c.get_id()] = c;
+    _courses.push_back(c);
 }
 
 void promo::add_week(week w) {
@@ -35,6 +35,10 @@ string promo::get_name() {
     return _name;
 }
 
+week& promo::get_week(int num_week) {
+    return _weeks[num_week];
+}
+
 void promo::list_promos() {
     int i;
     
@@ -42,6 +46,6 @@ void promo::list_promos() {
     cout << "\t Nom : " << get_name() << endl;
     cout << "\t Cours : " << endl;
     for(i = 0 ; i < _courses.size() ; i++) { 
-        cout << "\t\t" << _courses[i].get_name() << endl;
+        cout << "\t\t" << _courses.at(i).get_name() << endl;
     }
 }
