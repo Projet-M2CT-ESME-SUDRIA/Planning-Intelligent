ec7f733a07bdb45ef74a963eedd48f53c01ac8ba
BobleZob <corentin.cdy@live.fr>
2014-01-22 13:58:25 +0100

    ajout de la classe school

---
+++ Planning0.3/school.cpp
@@ -0,0 +1,77 @@
+/* 
+ * File:   school.cpp
+ * Author: Corentin
+ *
+ * Created on 22 janvier 2014, 13:16
+ */
+
+#include "school.h"
+#include "parseFile.h"
+#include <iostream>
+
+using namespace std;
+
+
+School::School(){
+    
+    parse_profs();
+    parse_courses();
+    parse_promos();
+}
+
+void School::parse_profs() {
+    
+    vector<string> line;
+    
+    line = read_file("profs.txt");
+    
+    for(int i=0 ; i<line.size() ; i++)
+    {
+        new_prof(line[i], _profs, _courses);
+    }
+    
+}
+
+void School::parse_courses() {
+    
+    vector<string> line;
+    
+    line = read_file("courses.txt");
+    for(int i=0 ; i<line.size() ; i++) {
+        new_course(line[i], _courses);
+    }
+    
+}
+
+void School::parse_promos() {
+    
+    vector<string> line;
+    
+    line = read_file("promos.txt");
+    
+    for(int i=0 ; i<line.size() ; i++) {
+        //promo.push_back(new_promo(line[i], courses));
+        new_promo(line[i], _promos, _courses);
+    }
+    
+}
+
+void School::display(){
+    
+    int s;
+    
+    s=_profs.size();
+    for(int i=0 ; i<s ; i++) {
+        _profs[i].list_profs();
+    }
+    
+    s=_courses.size();
+    for(int i=0 ; i<s ; i++) {
+        _courses[i].list_courses();
+    }
+    
+    s=_promos.size();
+    for(int i=0 ; i<s ; i++) {
+        _promos[i].list_promos();
+    }
+}
---
+++ Planning0.3/school.h
@@ -0,0 +1,33 @@
+/* 
+ * File:   school.h
+ * Author: Corentin
+ *
+ * Created on 22 janvier 2014, 13:15
+ */
+
+#ifndef SCHOOL_H
+#define	SCHOOL_H
+
+#include "course.h"
+#include "prof.h"
+#include "promo.h"
+
+class School {
+    
+public:
+    School();
+    
+    void parse_profs();
+    void parse_courses();
+    void parse_promos();
+    
+    void display();
+    
+private:
+    std::map<int, Course> _courses;
+    std::map<int, Prof> _profs;
+    std::map<int, Promo> _promos;
+};
+
+#endif	/* SCHOOL_H */
+
